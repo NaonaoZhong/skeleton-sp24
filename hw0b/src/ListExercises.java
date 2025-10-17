d1a79919caa977762a9dbd692e73a08489da3f5e
NaonaoZhong <2513794854@qq.com>
2025-10-18 07:31:15 +0800

    hw0b finished

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,66 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        if (L.size() == 0){
+            return 0;
+        }
+        int total = 0;
+
+        for(int i:L){
+            total = total + i;
+        }
+        return total;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> evenlst = new ArrayList<>();
+        for (int ele:L){
+            if (ele%2==0){
+                evenlst.add(ele);
+            }
+        }
+        return evenlst;
     }
-
+    public static boolean has(List<Integer> L,int a ){
+        for (int ele: L){
+            if (ele==a){
+                return true;
+            }
+        }
+        return false;
+    }
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+    /**write a very slow method before learning great datastructures */
+        List<Integer> common = new ArrayList<>();
+        for (int ele1 : L1){
+            if (has(common,ele1)){
+                continue;
+            }
+            if (has(L2,ele1)){
+                common.add(ele1);
+            }
+        }
+        return common;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int total = 0;
+        for (String word : words){
+            char[] word_Array = word.toCharArray();
+            for (char i : word_Array){
+                if(i == c){
+                    total = total + 1;
+                }
+            }
+        }
+        return total;
     }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,42 @@
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        TreeMap<Character,Integer> map = new TreeMap<>();
+        for (int i=1;i<=26;i++){
+            map.put(alphabet[i-1],i);
+        } 
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        TreeMap<Integer,Integer> squaremap = new TreeMap<>();
+        for (int num : nums){
+            squaremap.put(num,num*num);
+        }
+        return squaremap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        TreeMap<String,Integer> wordmap = new TreeMap<>();
+        for (String word : words){
+            if (wordmap.containsKey(word)){
+                wordmap.put(word,wordmap.get(word)+1);
+            }
+            else{
+                wordmap.put(word,1);
+            }
+        }
+        return wordmap;
     }
 }
