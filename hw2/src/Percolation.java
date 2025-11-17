a7493f7bd9a0f60eebd888b33893b2f2d3441609
NaonaoZhong <2513794854@qq.com>
2025-11-17 08:52:44 +0800

    hw2 finished

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,38 +1,84 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+   public class WeightedQuickUnionUF {
+        public WeightedQuickUnionUF(int n)      // creates a UF with n elements
+        public int count()                      // number of disjoint sets
+        public int find(int p)                  // the root of p's set
+        public boolean connected(int p, int q)  // whether p and q are in the same set
+        public void union(int p, int q)         // join p and q's sets together
+    }
+*/
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
-
+    // Add any necessary instance variables.
+    int numOfOpenSites;
+    int a;
+    int[] stateList;
+    WeightedQuickUnionUF uf1; // contains the bottom space
+    WeightedQuickUnionUF uf2; // do not contain the bottom
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        // Fill in this constructor.
+        if(N < 0){
+            throw new IllegalArgumentException();
+        }
+        a = N;
+        numOfOpenSites = 0;
+        stateList = new int[a*a];
+        uf1 = new WeightedQuickUnionUF(a*a + 2);
+        uf2 = new WeightedQuickUnionUF(a*a + 1);  
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        // Fill in this method.
+        valid(row, col);
+        int pos = row * a + col + 1;
+        
+        if(stateList[pos - 1] == 1){
+            return;
+        }
+        stateList[pos - 1] = 1;
+        // up right left down
+        if(col > 0 && isOpen(row, col - 1)){ uf1.union(pos, pos - 1); uf2.union(pos, pos - 1);}
+        if(col < a - 1 && isOpen(row, col + 1)){ uf1.union(pos, pos + 1); uf2.union(pos, pos + 1);}
+        if(row > 0 && isOpen(row - 1, col)){ uf1.union(pos, pos - a); uf2.union(pos, pos - a);}
+        else if(row == 0){ uf1.union(0, pos); uf2.union(0, pos); }
+        if(row < a - 1 && isOpen(row + 1, col)){ uf1.union(pos, pos + a); uf2.union(pos, pos + a); }
+        else if(row == a - 1){ uf1.union(pos, a*a + 1); }
+
+        numOfOpenSites ++;
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        // Fill in this method.
+        valid(row, col);
+        int pos = row * a + col + 1; 
+        return (stateList[pos - 1] == 1);
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        // Fill in this method.
+        valid(row, col);
+        int pos = row * a + col + 1; 
+        return uf2.connected(pos, 0);
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        // Fill in this method.
+        return numOfOpenSites;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
+        // Fill in this method.
+        return uf1.connected(0, a*a + 1);
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
-
+    public void valid(int row, int col){
+        if(row < 0 || row > a - 1){
+            throw new IndexOutOfBoundsException();
+        }
+        if(col < 0 || col > a - 1){
+            throw new IndexOutOfBoundsException();
+        }
+    }
 }
--- hw2/tests/PercolationTest.java
+++ hw2/tests/PercolationTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+// import static org.junit.jupiter.api.Assertions.fail;
 
 public class PercolationTest {
 
@@ -78,11 +78,64 @@ public class PercolationTest {
         assertThat(p.percolates()).isTrue();
     }
 
-    // TODO: Using the given tests above as a template,
-    //       write some more tests and delete the fail() line
+    //  Using the given tests above as a template,
+    //  write some more tests and delete the fail() line
     @Test
-    public void yourFirstTestHere() {
-        fail("Did you write your own tests?");
+    public void percolatesTest() {
+                int N = 5;
+        Percolation p = new Percolation(N);
+        // open sites at (r, c) = (0, 1), (2, 0), (3, 1), etc. (0, 0) is top-left
+        int[][] openSites = {
+                {0, 1},
+                {2, 0},
+                {3, 1},
+                {4, 1},
+                {1, 0},
+                {1, 1},
+                {3 ,0},
+        };
+        Cell[][] expectedState = {
+                {Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED}
+        };
+        for (int[] site : openSites) {
+            p.open(site[0], site[1]);
+        }
+        assertThat(getState(N, p)).isEqualTo(expectedState);
+        assertThat(p.percolates()).isTrue();
+    }
+
+    @Test
+    public void fullTest() {
+                int N = 5;
+        Percolation p = new Percolation(N);
+        // open sites at (r, c) = (0, 1), (2, 0), (3, 1), etc. (0, 0) is top-left
+        int[][] openSites = {
+                {0, 1},
+                {2, 0},
+                {3, 1},
+                {4, 1},
+                {4, 3},
+                {3, 3},
+                {1, 0},
+                {1, 1},
+                {3 ,0},
+        };
+        Cell[][] expectedState = {
+                {Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED, Cell.CLOSED},
+                {Cell.FULL, Cell.FULL, Cell.CLOSED, Cell.OPEN, Cell.CLOSED},
+                {Cell.CLOSED, Cell.FULL, Cell.CLOSED, Cell.OPEN, Cell.CLOSED}
+        };
+        for (int[] site : openSites) {
+            p.open(site[0], site[1]);
+        }
+        assertThat(getState(N, p)).isEqualTo(expectedState);
+        assertThat(p.percolates()).isTrue();
     }
 
 }
